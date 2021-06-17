a64be51bfd56cb9d82f5e5028c155caae15f8192
zhengpeng.li@outlook.com <zhengpeng.li@outlook.com>
2021-06-18 00:00:51 +0800

    添加zipkin用完成微服务链路追踪以及zipkin相关原理解释图

--- spring-cloud-api8000/src/main/java/com/lizhengpeng/api/ApiServer8000.java
+++ spring-cloud-api8000/src/main/java/com/lizhengpeng/api/ApiServer8000.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient //开启Eureka客户端服务(表示当前是一个Eureka客户端)
 @EnableFeignClients //开启客户端当前的Feign调用组件
-@EnableCircuitBreaker //Feign开启Hystrix支持时必须启用此注解(feign.hystrix.enable也必须开启否则FeignClient中fallBack不生效)
+//@EnableCircuitBreaker //Feign开启Hystrix支持时必须启用此注解(feign.hystrix.enable也必须开启否则FeignClient中fallBack不生效)
 @RestController
 @Slf4j
 public class ApiServer8000 {
